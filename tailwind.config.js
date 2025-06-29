c154e24c52edeecc0345442bad7641eb1af0b635
Maunducyrus <maunducyrus123@gmail.com>
2025-06-29 10:45:57 +0300

    npm uninstall tailwindcss - due to compatibility issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+// export default {
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js (ESM)
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Update paths
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

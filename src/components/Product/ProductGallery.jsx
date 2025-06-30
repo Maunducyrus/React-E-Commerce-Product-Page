ba2a68e0b622e6dd87b9ae335ca8c077cda1964e
Cyrus-M2 <devsben345@gmail.com>
2025-06-30 23:48:32 +0300

    productGallery

--- src/components/Product/ProductGallery.jsx
+++ src/components/Product/ProductGallery.jsx
@@ -1,3 +1,32 @@
-const ProductGallery = () => <div>Product Gallery</div>;
+// const ProductGallery = () => <div>Product Gallery</div>;
 
-export default ProductGallery;
+// export default ProductGallery;
+// src/components/Product/ProductGallery.jsx
+import { useState } from 'react';
+
+export default function ProductGallery({ images }) {
+  const [selected, setSelected] = useState(images[0]);
+
+  return (
+    <div>
+      <img
+        src={selected}
+        alt="Main"
+        className="w-full h-96 object-cover rounded-lg hover:scale-105 transition-transform"
+      />
+      <div className="flex gap-2 mt-4">
+        {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt={`thumb-${i}`}
+            onClick={() => setSelected(img)}
+            className={`h-20 w-20 object-cover rounded border cursor-pointer ${
+              selected === img ? 'border-blue-500' : 'border-gray-200'
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}

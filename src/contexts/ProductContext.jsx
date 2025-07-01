a25a502a07f15332ba6e308ca921e8031461e3ab
Maunducyrus <maunducyrus123@gmail.com>
2025-07-02 00:00:28 +0300

    PRoductContext

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,3 +1,23 @@
-import { createContext } from "react";
+// import { createContext } from "react";
+
+// export const ProductContext = createContext();
+import { createContext, useState, useEffect } from 'react';
+import { products as staticProducts } from '../data/products';
 
 export const ProductContext = createContext();
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(staticProducts);
+  }, []);
+
+  const getProductById = (id) => products.find((p) => p.id === id);
+
+  return (
+    <ProductContext.Provider value={{ products, getProductById }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};

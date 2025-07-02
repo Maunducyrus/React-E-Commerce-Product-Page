7b660d6acad9ab6a3b24931ecf7d25cd8c95aa75
Maunducyrus <maunducyrus123@gmail.com>
2025-07-02 23:13:34 +0300

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/main.css';
+import { CartProvider } from './contexts/CartContext';
+import { WishlistProvider } from './contexts/WishlistContext';
+import { UserProvider } from './contexts/UserContext';
+import { ProductProvider } from './contexts/ProductContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <ProductProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <App />
+          </WishlistProvider>
+        </CartProvider>
+      </ProductProvider>
+    </UserProvider>
   </React.StrictMode>
 );

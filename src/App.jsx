feb32c2230728611ace9dfc17c0dc0ac5e7961ec
Maunducyrus <maunducyrus123@gmail.com>
2025-07-03 01:03:19 +0300

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navigation/Navbar'
-import Header from './components/Navigation/Header'
+// import { useState } from 'react'
+// import './App.css'
+// import Navbar from './components/Navigation/Navbar'
+// import Header from './components/Navigation/Header'
+
+// function App() {
+
+//   return (
+//    <div>
+//     <h3>Hello React</h3>
+//     < Navbar/>
+//     < Header/>
+//    </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import AppRoutes from '../routes/AppRoutes';
+import Header from './components/Navigation/Header';
+import Footer from './components/Navigation/Footer';
+import Toast from './components/UI/Toast';
 
 function App() {
-
   return (
-   <div>
-    <h3>Hello React</h3>
-    < Navbar/>
-    < Header/>
-   </div>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <AppRoutes />
+      </main>
+      <Footer />
+      <Toast />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '../styles/main.css';
+
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
+import { WishlistProvider } from './contexts/WishlistContext';
+import { UserProvider } from './contexts/UserContext';
+import { ProductProvider } from './contexts/ProductContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <ProductProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <App />
+            </WishlistProvider>
+          </CartProvider>
+        </ProductProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);

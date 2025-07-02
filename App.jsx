9fa0006234e8e47bcf48a6672e3588434d7a46a9
Maunducyrus <maunducyrus123@gmail.com>
2025-07-02 23:14:47 +0300

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,35 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
-import "./styles/main.css";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "./routes/AppRoutes";
+// import "./styles/main.css";
+
+// function App() {
+//   return (
+//     <Router>
+//       <AppRoutes />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import Header from './components/Navigation/Header';
+import Footer from './components/Navigation/Footer';
+import Toast from './components/UI/Toast';
 
 function App() {
   return (
     <Router>
-      <AppRoutes />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <AppRoutes />
+        </main>
+        <Footer />
+        <Toast />
+      </div>
     </Router>
   );
 }

11044b33c2efc9fc50cd1b957a20e85eb25ebf38
Maunducyrus <maunducyrus123@gmail.com>
2025-07-02 23:03:11 +0300

    utils

--- utils/formatPrice.js
+++ utils/formatPrice.js
@@ -1,3 +1,10 @@
-export function formatPrice(price) {
-  return `$${price.toFixed(2)}`;
+// export function formatPrice(price) {
+//   return `$${price.toFixed(2)}`;
+// }
+
+export function formatPrice(value) {
+  return new Intl.NumberFormat('en-KE', {
+    style: 'currency',
+    currency: 'KES'
+  }).format(value);
 }

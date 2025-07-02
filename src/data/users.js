5b289877104d965d5efd15c2c077c8c99fcec10d
Maunducyrus <maunducyrus123@gmail.com>
2025-07-02 22:53:54 +0300

    users

--- src/data/users.js
+++ src/data/users.js
@@ -1,1 +1,18 @@
-export const users = [];
+export const users = [
+  {
+    id: 1,
+    username: 'john_doe',
+    email: 'john@example.com',
+    password: 'hashedpassword',
+    address: '123 Main St, Nairobi',
+    role: 'customer'
+  },
+  {
+    id: 2,
+    username: 'admin_user',
+    email: 'admin@example.com',
+    password: 'hashedadminpassword',
+    address: '456 Admin Blvd, Nairobi',
+    role: 'admin'
+  }
+];
